83f79ba8801fad1ad3eeed1b3fa06dc44a6aa8b2
d9eck <daniel.co.so@gmail.com>
2017-11-04 17:24:12 +0100

    Update set-up.sh

--- set-up.sh
+++ set-up.sh
@@ -5,17 +5,17 @@ find pub/media -type f -exec chmod g+w {} \;
 find pub/media -type d -exec chmod g+ws {} \;
 
 
-mv wwwmag/app/design/frontend/Smartwave /bitnami/magento/htdocs/app/design/frontend/
+mv app/design/frontend/Smartwave /bitnami/magento/htdocs/app/design/frontend/
 ln -sf /bitnami/magento/htdocs/app/design/frontend/Smartwave /opt/bitnami/magento/htdocs/app/design/frontend/Smartwave
-mv wwwmag/app/code /bitnami/magento/htdocs/app/
+mv app/code /bitnami/magento/htdocs/app/
 ln -sf /bitnami/magento/htdocs/app/code /opt/bitnami/magento/htdocs/app/code
-mv wwwmag/pub/media/js /bitnami/magento/htdocs/pub/media/
+mv pub/media/js /bitnami/magento/htdocs/pub/media/
 ln -sf /bitnami/magento/htdocs/pub/media/js /opt/bitnami/magento/htdocs/pub/media/js
-mv wwwmag/pub/media/logo /bitnami/magento/htdocs/pub/media/
+mv pub/media/logo /bitnami/magento/htdocs/pub/media/
 ln -sf /bitnami/magento/htdocs/pub/media/logo /opt/bitnami/magento/htdocs/pub/media/logo
-mv wwwmag/pub/media/porto /bitnami/magento/htdocs/pub/media/
+mv pub/media/porto /bitnami/magento/htdocs/pub/media/
 ln -sf /bitnami/magento/htdocs/pub/media/porto /opt/bitnami/magento/htdocs/pub/media/porto
-mv wwwmag/pub/media/wysiwyg /bitnami/magento/htdocs/pub/media/
+mv pub/media/wysiwyg /bitnami/magento/htdocs/pub/media/
 ln -sf /bitnami/magento/htdocs/pub/media/wysiwyg /opt/bitnami/magento/htdocs/pub/media/wysiwyg
 
 cd /opt/bitnami/magento/htdocs
